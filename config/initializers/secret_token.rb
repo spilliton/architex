b5fe304a54ea7e339ec2457bd49b478f0d526b98
spilliton <kloeppingzd@gmail.com>
2012-12-26 12:02:22 -0600

    new rails project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Architex::Application.config.secret_token = '598bc46db392dec124818ec0282bdd077f2fff5a05b03594f47864b086fd0bf37d3bb824e0c231d20e9671d4abae249be17edac71674c444ad25061d892ccbd3'
